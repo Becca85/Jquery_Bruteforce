e74077dac0b23529ed6fe266b10a3d78675c1068
Becca <rebeccamaccio@gmail.com>
2016-07-30 16:46:04 +0200

    tentative echouée

---
+++ fichierslies/script.js
@@ -0,0 +1,44 @@
+jQuery(document).ready(function(){  
+	
+$("#chercher").click(function(){
+
+var brutDeForce = $.ajax({
+	url : 'http://docusland.fr/code-academie/js_bruteforce/',
+	type : 'POST',
+	dataType : 'html',
+}); // appel ajax du site a craquer
+var tabDeMot= $.ajax({
+	url:'http://docusland.fr/code-academie/js_bruteforce/dico.txt',
+	type : 'GET',
+	dataType : 'text'
+});// appel ajax du dico
+
+	function decouvrirmotdepasse() {
+			tabDeMot.split();// decoupage de la liste de mot en tableau
+			for (var i = 0; i < tabDeMot.length; i++){
+				if("brutDeForce + '?password=' + tabDeMot[i] = 'success'"){
+					return tabDeMot[i];
+				}
+
+			}
+	});
+
+});
+
+// tentative d'utiliser get et post au lieu d'ajax
+ /*$("#chercher").click(function(){
+
+
+var i = 0;
+$.get({
+	'http://docusland.fr/code-academie/js_bruteforce/dico.txt',
+	'false', 
+	function tabDeMot,
+	'text',
+});
+$.post({
+	'http://docusland.fr/code-academie/js_bruteforce/',
+	 function decouvrirmotdepasse,
+	'html',
+});*/
+ 
---
+++ script.js
@@ -0,0 +1,26 @@
+
+$('sel').load('http://docusland.fr/code-academie/js_bruteforce/dico.txt', function() {
+  //une ou plusieurs instructions
+  alert('La première zone a été mise à jour');
+  
+});
+
+
+
+
+/*var i = 0
+
+var tableaudemot;
+
+fonction decouvrirmotdepasse
+
+$.ajax({ 
+        url      : http://docusland.fr/code-academie/js_bruteforce/dico.txt,
+        decouper les elements du dico en tableau
+        return tableaudemot
+
+
+
+     	for(tester mot par mot)
+		if retour est bon = url : http://docusland.fr/code-academie/js_bruteforce/
+		retourner le mot*/
